709aa74fb8e4efda528eead7dd77c90e0239c868
Karim Rahal <135629176+Karim-rahal@users.noreply.github.com>
2025-04-13 20:43:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,3 +14,6 @@ app.use('/api/auth', require('./routes/authRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+    res.send('API is running ✅');
+  });
