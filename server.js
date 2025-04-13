2133c4fca9ceaa71cfa404aa4b8ac854a9d22b97
Karim Rahal <135629176+Karim-rahal@users.noreply.github.com>
2025-04-13 20:51:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const cors = require('cors');
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+
+
+const allowedOrigins = [
+  'https://fams-nceccijsl-karim-rahals-projects-519f35ea.vercel.app',
+  'http://localhost:3000'
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
+
 app.use(express.json());
 
+// Routes
 app.use('/api/auth', require('./routes/authRoutes'));
+
+// Root
+app.get('/', (req, res) => {
+  res.send('API is running ✅');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-app.get('/', (req, res) => {
-    res.send('API is running ✅');
-  });
